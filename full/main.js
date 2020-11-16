8297333449253812a3642de4442cc4f40535c890
help-hcs <73950539+help-hcs@users.noreply.github.com>
2020-11-16 22:33:43 +0530

    Update main.js

--- full/main.js
+++ full/main.js
@@ -1,5 +1,24 @@
+window.onbeforeunload = function (e) {
+    // Cancel the event
+    e.preventDefault();
 
-$(document).bind('keydown', 'ctrl+s', function(e) {
+    // Chrome requires returnValue to be set
+    e.returnValue = 'Really want to quit the game?';
+};
+
+//Prevent Ctrl+S (and Ctrl+W for old browsers and Edge)
+document.onkeydown = function (e) {
+    e = e || window.event;//Get event
+
+    if (!e.ctrlKey) return;
+
+    var code = e.which || e.keyCode;//Get key code
+
+    switch (code) {
+        case 83://Block Ctrl+S
+        case 87://Block Ctrl+W -- Not work in Chrome and new Firefox
             e.preventDefault();
-            return false;
-        });
+            e.stopPropagation();
+            break;
+    }
+};
